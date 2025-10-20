be5c20a3032675eecd03e2b11ec4e62d3a208beb
Joaquin <jearce@fundacionparaguaya.org.py>
2025-10-20 08:14:32 -0300

    Fix: Configurar CORS para permitir peticiones desde Vercel

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,19 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
-  new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-  })
-);
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
 
-
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://tesis-fonokids-joaquin.vercel.app',
+      'http://localhost:4200'
+    ],
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
