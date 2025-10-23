2273191cbe0e7584d3790580e87faef15ef87f82
Joaquin <jearce@fundacionparaguaya.org.py>
2025-10-23 01:19:06 -0300

    Mejoras en el Chatbot otra vez

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     })
   );
 
-  const port = process.env.PORT || 8080; // 👈 Cambiar de 3000 a 8080
+  const port = process.env.PORT || 8080; // 👈 Cambiar de 3000 a 8g080
   await app.listen(port, '0.0.0.0');
   
   console.log(`🚀 Application is running on port ${port}`);
