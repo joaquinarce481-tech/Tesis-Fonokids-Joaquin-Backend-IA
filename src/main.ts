9e359d5598bc0cb76f0568269564147844059662
Joaquin <jearce@fundacionparaguaya.org.py>
2025-10-23 18:34:54 -0300

    Implementar proxy serverless en Vercel

--- src/main.ts
+++ src/main.ts
@@ -62,7 +62,7 @@ async function bootstrap() {
     })
   );
 
-  const port = process.env.PORT || 8080;
+  const port = process.env.PORT || 3000;
   
   await app.listen(port, '0.0.0.0', () => {
     console.log('='.repeat(50));
