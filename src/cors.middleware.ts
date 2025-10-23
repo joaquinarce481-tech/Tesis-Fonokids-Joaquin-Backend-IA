e3a3d3bbab78879a5bd63a9ece3270ab9d241599
Joaquin <jearce@fundacionparaguaya.org.py>
2025-10-23 01:26:28 -0300

    Mejoras en el Chatbot otra vez

--- src/cors.middleware.ts
+++ src/cors.middleware.ts
@@ -4,15 +4,25 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    res.header('Access-Control-Allow-Origin', '*');
+    const allowedOrigins = [
+      'https://tesis-fonokids-joaquin.vercel.app',
+      'http://localhost:4200'
+    ];
+    
+    const origin = req.headers.origin as string;
+    
+    if (origin && allowedOrigins.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+      res.header('Access-Control-Allow-Credentials', 'true');
+    }
+    
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
 
     if (req.method === 'OPTIONS') {
-      res.sendStatus(200);
-    } else {
-      next();
+      return res.sendStatus(200);
     }
+    
+    next();
   }
 }
